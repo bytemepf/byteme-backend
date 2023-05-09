a689c686de620b45a8e9fc266a303b6affec7168
Daniel Durán <danieldurange@outlook.com>
2023-05-09 13:21:48 -0500

    products.router.js: correción en la exportación

--- router/products.controller.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const products = Router();
-
-products.get("/products", get);
-
-module.exports = {
-  auth: products,
-};
--- router/router.js
+++ router/router.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const { auth } = require("./auth.router");
+const { products } = require("./products.router");
 
 const router = Router();
 
 router.use("/api/auth", auth);
-router.use("/api/products", auth);
+router.use("/api/products", products);
 
 module.exports = {
   router,
