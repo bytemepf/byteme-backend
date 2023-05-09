8b7a31311812d0401af316c22b10eead86d89197
Daniel Durán <danieldurange@outlook.com>
2023-05-09 13:12:22 -0500

    Modelo User creado

---
+++ database/connection.js
@@ -0,0 +1,5 @@
+const { Sequelize } = require("sequelize");
+
+const database = new Sequelize(process.env.DB_URL, { logging: false });
+
+module.exports = { database };
---
+++ models/User.model.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+const { database } = require("../database/connection");
+
+const User = database.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: "USER_ROLE",
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    google: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  { timestamps: false }
+);
+
+module.exports = {
+  User,
+};
