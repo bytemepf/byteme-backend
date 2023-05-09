89ac7d52af2a3dc901f52787714eaa8c59527ae6
Daniel Durán <danieldurange@outlook.com>
2023-05-09 08:54:53 -0500

    Estructura del servidor ampliada

--- router/router.js
+++ router/router.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
+const { auth } = require("./auth.router");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json({ ruta: "/" });
-});
+router.use("/api/auth", auth);
+router.use("/api/products", auth);
 
 module.exports = {
   router,
