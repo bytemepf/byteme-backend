0a84c6f82abee3eb493a0535a5dd20922aa9a3fc
matiass42 <matiassvelazquez@gmail.com>
2023-05-10 13:51:28 -0300

    getproducts y delete fisico

--- controllers/products/getProducts.controller.js
+++ controllers/products/getProducts.controller.js
@@ -1,5 +1,26 @@
+const { Products} = require('../database');
+
+
 const getProducts = (req, res) => {
-  res.json({ ruta: "get/products" });
+  try{
+   const {name}=req.query;
+   const getallproducts = Products.findAll()
+ 
+   if(name){
+     const ProductName = getallproducts.filter((r) =>
+           r.name.toLowerCase() === name.toLowerCase());
+ 
+         ProductName.length > 0  ? res.send(ProductName) 
+                                 : res.status(404).send( "no product found" );
+
+  }else{
+    res.status(200).send(getallproducts)
+  }
+  }catch{
+    res.status(400).send("not found")
+}
+
+  //res.json({ ruta: "get/products" });
 };
 
 module.exports = {
--- router/products.router.js
+++ router/products.router.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+const getProducts = require('../controllers/products/getProducts.controller')
+const DeleteProduct = require('../controllers/products/deleteProducts.controller')
 
 const products = Router();
 
-products.get("/products", get);
+products.get("/products", getProducts);
+products.delete("/delete/:id",DeleteProduct)
 
 module.exports = {
-  products,
+  products
 };
