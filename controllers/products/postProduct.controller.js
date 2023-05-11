4d717779fec92faf4c6c564d00cb153fd930a7c9
Daniel Durán <danieldurange@outlook.com>
2023-05-11 05:33:15 -0500

    .env.template

--- controllers/products/postProduct.controller.js
+++ controllers/products/postProduct.controller.js
@@ -3,20 +3,13 @@ const { Product } = require("../../models/Product.model");
 const fs = require("fs-extra");
 
 const postProduct = async (req, res) => {
-  let imageUrl = "";
-  let publicIdImage = "";
-
   if (req.files?.image) {
-    const { secure_url, public_id } = await uploadImage(
-      req.files.image.tempFilePath
-    );
-    imageUrl = secure_url;
-    publicIdImage = public_id;
+    const { secure_url } = await uploadImage(req.files.image.tempFilePath);
+    const product = await Product.create({ ...req.body, image: secure_url });
+    await fs.unlink(req.files.image.tempFilePath);
+    return res.json(product);
   }
-  const product = await Product.create({ ...req.body, image: imageUrl });
-
-  await fs.unlink(req.files.image.tempFilePath);
-  res.json(product);
+  return res.status(400).json({ message: "Se requiere una imagen" });
 };
 
 module.exports = {
