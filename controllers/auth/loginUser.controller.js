c0c1c12c5cc53f3479d3b39a3888f60ef97ff8e8
Daniel Duran <danielduranje@outlook.com>
2023-05-11 14:35:02 -0500

    error corregido en login

--- controllers/auth/loginUser.controller.js
+++ controllers/auth/loginUser.controller.js
@@ -4,7 +4,6 @@ const { generateJWT } = require("../../helpers/generateJWT");
 
 const loginUser = async (req, res) => {
   let { email, password } = req.body;
-
   try {
     const user = await User.findOne({ where: { email } });
 
@@ -15,9 +14,14 @@ const loginUser = async (req, res) => {
     }
 
     const validPassword = bcryptjs.compareSync(
-      toString(password),
+      password,
       user.password
     );
+    console.log({password, passworddb: user.password ,bool: bcryptjs.compareSync(
+      password,
+      user.password
+    )})
+    
     if (!validPassword) {
       return res.status(400).json({ message: "Contraseña incorrecta" });
     }
