3c614332e95eb042af15e42d71e9374d5bedee97
danivalles94 <cdvallesb7@gmail.com>
2023-05-20 23:07:24 -0500

    changes nodemailer

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -1,38 +1,53 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+import { User } from '../models';
 
- const sendEmail = async ()=>{
+module.exports = (app)=>{
 
-//constante de configuracion del correo y puerto del que se enviaran los correos 
-    
-    let transporter = {
-        host: 'smtp.gmail.com',
-        port: 587,
-    // Se cambiara esta parte por variables de entorno (.env)
-        auth: {
-            user: `${process.env.EMAIL_ADDRESS}`,
-            pass: `${process.env.EMAIL_PASSWORD}`
+    app.post("/mailpurcharse", (req, res) =>{
+        if(req.body.email === ""){
+            res.status(400).send("email required")
         }
-    }
+        console.error(req.body.email)
+
+        User.findOne({
+            where: {
+                email: req.body.email
+            },
+        }).then((user) => {
+            if(user === null){
+                console.error("email not in database");
+                res.status(403).send("email not in db")
+            } else {
+    //constante de configuracion del correo y puerto del que se enviaran los correos  
+                const transporter = nodemailer.createTransport({
+                    host: 'smtp.gmail.com',
+                    port: 587,
+                    auth: {
+                        user: `${process.env.EMAIL_ADDRESS}`,
+                        pass: `${process.env.EMAIL_PASSWORD}`
+                    }
+                });
 
     //En esta variable message se pondra la plantilla de Stripo (en caso tal se modularizara para mayor orden)
-    let message = {
-        from: 'bytemepf@gmail.com',
-        //Se cambiara por el correo al cual se debera enviar el message
-        to: 'cdvallesb7@gmail.com',
-        subject: 'Test two',
-        text: 'relevant info about email of shopping',
-    }
-
-     const transport = nodemailer.createTransport(transporter);
-
-     const info = await transport.sendMail(message);
-
-     console.log(info)
-}
-
-sendEmail();
-
-module.exports = {
-    sendEmail,
+                let message = {
+                    from: 'bytemepf@gmail.com',
+                    to: `${user.email}`,
+                    subject: 'purchase resume',
+                    //Aqui debe traerse la infomacion de la compra para enviarla como cuerpo del correo     
+                    text: 'relevant info about email of shopping',
+                };
+                console.log('sending email');
+                
+                transporter.sendMail(mailOptions, (error, response) =>{
+                    if(error) {
+                        console.error('There was an error', error);
+                    } else {
+                        console.log('here is the response', response);
+                        res.status(200).json('recovery email sent')
+                    }
+                });
+            }
+        })
+    })
 }
