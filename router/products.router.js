35296c041d2bafaa8811001a95440d149120cfd3
VallesCris <cdvallesb7@gmail.com>
2023-05-24 09:40:29 -0500

    routes

--- router/products.router.js
+++ router/products.router.js
@@ -1,14 +1,16 @@
 // Importaciones de terceros (NPM)
 const { Router } = require("express");
-
 // Importaciones locales (Controladores)
 const { getAllProducts, getProductsByName, filterProducts, getProductById } = require("../controllers");
 
-
 const products = Router();
 
+products.get("/", getAllProducts);
+products.get("/search", getProductsByName);
+products.get("/filter", filterProducts)
+products.get("/:id", getProductById)
 products.get("/products");
 
 module.exports = {
   products
-};
+}
