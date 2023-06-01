b3266a0a16ae91218e86a1e5d3be3376f9d8ad73
matiass42 <matiassvelazquez@gmail.com>
2023-06-01 01:44:16 -0300

    listo?

--- middlewares/nodemailer.js
+++ middlewares/nodemailer.js
@@ -3,40 +3,35 @@ const nodemailer =  require("nodemailer");
 
 async function nodemailerPay(recipient, total) {
 
-  const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      auth: {
-          user:` bytemepf@gmail.com`,//`${process.env.EMAIL_ADDRESS}`,
-          pass: `watdzbhmsmnxcenp`//`${process.env.EMAIL_PASSWORD}`
-      }
-  });
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: `${process.env.EMAIL_ADDRESS}`,
+            pass: `${process.env.EMAIL_PASSWORD}`
+        }
+    });
 
+    const mail = await transporter.sendMail({
+      from: 'bytemepf@gmail.com',
+      to: recipient,
+      subject: `Compra ✔` ,
 
-
-  const mail = await transporter.sendMail({
-    from: 'bytemepf@gmail.com',
-    to: recipient,
-    subject: `Compra ✔` ,
-
-    html: `<center>
+      html: `<center>
 |              <div width=300px style="font-size: 2rem; background-color: #dfced5; height: 65rem; width: 83rem;"> 
-              <br>
-              <h3>Hola ${recipient} </h3> 
-              <br>
+                <br>
+                <h3>Hola ${recipient} </h3> 
+                <br>
 
-              <h3>Recibimos correctamente tu compra</h3>
-              <h2>$${total}</h2>
-              <br>
-              <label>Muchas gracias!! Equipo BYTE ME</label>
-            </div>
-          </center>`
-  });
-  console.log(mail, '!!!!!');
-} 
-
-
-
+                <h3>Recibimos correctamente tu compra</h3>
+                <h2>$${total}</h2>
+                <br>
+                <label>Muchas gracias!! Equipo BYTE ME</label>
+              </div>
+            </center>`
+    });
+    console.log(mail, '!!!!!');
+  } 
   module.exports = {
     nodemailerPay,
   }
