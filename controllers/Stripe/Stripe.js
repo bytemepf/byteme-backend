175dde578c515fe880df21fcd9619fa51b05c7e1
matiass42 <matiassvelazquez@gmail.com>
2023-06-01 01:48:13 -0300

    listo?

--- controllers/Stripe/Stripe.js
+++ controllers/Stripe/Stripe.js
@@ -1,4 +1,5 @@
-// Set your secret key. Remember to switch to your live secret key in production.
+// Set your secret key. Remember to switch to your live secret key in production
+
 // See your keys here: https://dashboard.stripe.com/apikeys
 const stripe = require('stripe')('sk_test_51N6grFDH4FxWaNHv6qZoPtl55F3AApQtxzDxDI88jr4n9TEI0G5bdeUmkUiZlu6vUesJgFAYbEMXbO9t7hXL5ynX00wD7WhVow');
 const { nodemailerPay } = require('../../middlewares/nodemailer');
@@ -10,7 +11,9 @@ const calculateTotalAmount = (quantity,price) => {
 };
 
 const paymentStripe = async (req, res, next) => {
-  const {email, quantity,price} = req.body;
+  const { email, quantity, price} = req.body;
+
+  console.log(email);
 
   await stripe.paymentIntents.create(
     {
@@ -18,18 +21,49 @@ const paymentStripe = async (req, res, next) => {
       amount: calculateTotalAmount(quantity,price),
       currency: 'USD',
      //payment_method: 'pm_card_visa',
-     automatic_payment_methods: {
+      automatic_payment_methods: {
       enabled: true
     },
-  },
-  (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      next(new Error('Error in paymentStripe' + stripeErr))
-    } else {
-      res.status(200).json(stripeRes)
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        next(new Error('Error in paymentStripe' + stripeErr))
+      } else {
+        res.status(200).json(stripeRes)
+      }
     }
+  )
+  await nodemailerPay(email, price); 
   }
-)
-await nodemailerPay(email, price); 
-}
 module.exports = paymentStripe
+//const stripe = require('stripe')('sk_test_51N6grFDH4FxWaNHv6qZoPtl55F3AApQtxzDxDI88jr4n9TEI0G5bdeUmkUiZlu6vUesJgFAYbEMXbO9t7hXL5ynX00wD7WhVow');
+//
+//const calculateTotalAmount = (quantity,price) => {
+//  const pricePerItem = price * 100
+//  const totalAmount = quantity * pricePerItem;
+//  return totalAmount;
+//};
+//
+//const paymentStripe = async (req, res, next) => {
+//  const { quantity,price} = req.body;
+//
+//  await stripe.paymentIntents.create(
+//    {
+//     // source: tokenId,
+//      amount: calculateTotalAmount(quantity,price),
+//      currency: 'USD',
+//     //payment_method: 'pm_card_visa',
+//     automatic_payment_methods: {
+//      enabled: true
+//    },
+//    },
+//    (stripeErr, stripeRes) => {
+//      if (stripeErr) {
+//        next(new Error('Error in paymentStripe' + stripeErr))
+//      } else {
+//        res.status(200).json(stripeRes)
+//      }
+//    }
+//  )}
+//
+//module.exports = paymentStripe
