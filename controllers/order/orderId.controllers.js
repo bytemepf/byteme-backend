5622f7f7efc5a296911199c6365d42cb4a629a90
matiass42 <matiassvelazquez@gmail.com>
2023-06-01 00:31:22 -0300

    listo?

--- controllers/order/orderId.controllers.js
+++ controllers/order/orderId.controllers.js
@@ -3,20 +3,20 @@ const {Cart} = require("../../models/Cart")
 
 
 const getOrdersById = async (req, res) => {
-    const userId = req.params.userId;
-  
-    try {
-      const orders = await Order.findAll({ where: { user: userId } });
-  
-      if (orders.length > 0) {
-        res.status(200).send(orders);
-      } else {
-        res.status(400).send("No se encontraron órdenes para el usuario especificado");
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Ocurrió un error al obtener las órdenes");
+  try {
+    const orders = await Order.findOne({
+      order: [['createdAt', 'DESC']],
+    });
+    console.log(orders);
+    if (orders) {
+      res.status(200).json(orders);
+    } else {
+      res.status(401).send("No se encontraron órdenes para el usuario especificado");
     }
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Ocurrió un error al obtener las órdenes");
+  }
   };
 
 module.exports = {
