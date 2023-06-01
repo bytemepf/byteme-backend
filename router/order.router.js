2ed3c4503b47eab99051f8235a148529bcb780df
matiass42 <matiassvelazquez@gmail.com>
2023-06-01 03:23:46 -0300

    listo?

--- router/order.router.js
+++ router/order.router.js
@@ -2,17 +2,18 @@ const { Router } = require("express");
 const {postorderuser}=require("../controllers/order/postorder.controller")
 const {getOrdersById} = require("../controllers/order/orderId.controllers")
 const {getAll} = require("../controllers/order/getallorder")
-const {getOrdersByIduser} = require("../controllers/order/allorderuser")
+//const {getOrdersById} = require("../controllers/order/allorderuser")
+
 const order = Router();
 
 //Todas las órdenes
 order.get("/", getAll);
 //Todas las órdenes de un usuario
-order.get("/:userId", getOrdersById );
+//order.get("/:userId", getOrdersById );
 //crear orden por usuario
 order.post("/:userId",postorderuser)
 //Traer una orden en particular
-order.get("/pagar",getOrdersByIduser );
+order.get("/pagar", getOrdersById );
 //Modificar una orden en particular
 order.put("/order/:orderId", );
 
