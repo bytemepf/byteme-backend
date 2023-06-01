e59094978dd8b34baefc531c46a78d3a1655e729
matiass42 <matiassvelazquez@gmail.com>
2023-06-01 00:26:19 -0300

    solucionado?

--- controllers/order/postorder.controller.js
+++ controllers/order/postorder.controller.js
@@ -4,25 +4,59 @@ const { Product } = require("../../models/Product.model");
 const {User}=require("../../models/User.model")
 
 const postorderuser = async (req,res)=>{
-    try {
-      const {adress,phone,city,country}=req.body
-     const Cart_id = req.params
-     const user_id = req.params
-     const nameuser= await User.findByPk(user_id)
-     console.log(nameuser)
-     const carroaorden= await Cart.findByPk(Cart_id)
-     console.log(carroaorden)
-     const neworder= await Order.create(
-      {
-        detail:Cart_id.productid,
-        adress: adress,
-        phone:phone,
-        city: city,
-        country:country,
-        name:nameuser.name,
-        user_id:user_id,
-        total:Cart_id.total
-      }
+  try {
+    const {adress,phone,city,country,productC,name}=req.body
+  // const Cart_id = req.params
+   const userId = req.params.userId
+  // console.log(req.params)
+   const nameuser= await User.findOne({where:{email:userId}})
+ //  const nameep=[
+ //   {
+ //     id: '8200dd15-186b-4fd0-a92e-af8be2d9c08f',
+ //     name: 'Grabadora de voz digital',
+ //     description: 'Grabadora portátil de voz digital con amplio almacenamiento y alta calidad de grabación.',
+ //     brand: 'Olympus',
+ //     price: 79.99
+ //   },
+ //   {
+ //     id: '8200dd15-186b-4fd0-a92e-af8be2d9c08f',
+ //     name: 'Grabadora de voz digital',
+ //     description: 'Grabadora portátil de voz digital con amplio almacenamiento y alta calidad de grabación.',
+ //     brand: 'Olympus',
+ //     price: 79.99
+ //   }
+ // ]
+//   const cartALL = await Cart.findAll({where:{user:userId}})
+  //const Namep= JSON.parse([productC])
+//const idP = productC.map((producto) => producto.id);
+  //const nameP = productC.map((producto) => producto.name);
+//    const nombresProductos = [];
+// for (let i = 0; i < cartALL.length; i++) {
+//   nombresProductos.push(cartALL[i].productname);
+// }
+//console.log(productC)
+var sumaname = [];  
+for (let i = 0; i < productC.length; i++) {
+  sumaname.push(productC[i].name);
+  console.log(productC[1])
+}
+  var sumaTotalC = 0;  
+for (let i = 0; i < productC.length; i++) {
+  sumaTotalC += productC[i].price;
+}
+ //  const carduserall= await cartALL.length
+ //  console.log(carduserall)
+  const neworder= await Order.create(
+    {
+      detail:sumaname.join(','),
+      adress: adress,
+      phone:phone,
+      city: city,
+      country:country,
+      name:name,
+      userId:userId,
+      total:sumaTotalC
+    }
      )
      res.status(200).json(neworder)  
       } catch  {
